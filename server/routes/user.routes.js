7b404c48a49aad707661c32179d8dcc95e651fde
Himadri Roy <141954844+odincoder1605@users.noreply.github.com>
2025-05-04 10:29:19 +0530

    controller to show favorites added

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { verifyToken } from "../middlewares/verifyToken.js"
-import {followUser, getDetails,updateAvatar} from '../controllers/user.controllers.js';
+import { verifyToken} from "../middlewares/verifyToken.js"
+import {followUser, getDetails,updateAvatar,getUserFavorites} from '../controllers/user.controllers.js';
 import upload from "../middlewares/multer.js";
+
 
 const router = express.Router();
 router.put('/follow/:id',verifyToken,followUser);
 router.get('/details/:id',verifyToken,getDetails);
 router.put('/update/avatar/:id',verifyToken,upload.single('avatar'),updateAvatar);
+router.get('/showFavorites',verifyToken,getUserFavorites)
 export default router;
